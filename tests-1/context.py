e7fdedf68bc0ab0c25a9175fff7080856493fb92
Jackson Chadfield <ibjacksonc@hotmail.com>
2017-02-05 19:33:53 +1300

    Edited tests/context.py To Fix A CWD Problem

--- tests-1/context.py
+++
@@ -1,9 +0,0 @@
-'''
-Allows The Code To Be Easily Tested Without Having To Install First
-'''
-
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
-
-import smcAPI
--- tests/context.py
+++ tests/context.py
@@ -4,6 +4,6 @@ Allows The Code To Be Easily Tested Without Having To Install First
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(__file__+'/../..'))
 
 import smcAPI
